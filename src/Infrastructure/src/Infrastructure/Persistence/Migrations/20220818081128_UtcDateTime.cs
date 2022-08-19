344712fa533039d7219262b6404f53c1404baa03
Kit086 <kit.j.lau@outlook.com>
2022-08-20 00:17:45 +0800

    Docs: Migration and configuration files

---
+++ src/Infrastructure/src/Infrastructure/Persistence/Migrations/20220818081128_UtcDateTime.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Infrastructure.src.Infrastructure.Persistence.Migrations
+{
+    public partial class UtcDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                table: "TodoLists",
+                newName: "LastModifiedUtc");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "TodoLists",
+                newName: "CreatedUtc");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                table: "TodoItems",
+                newName: "LastModifiedUtc");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "TodoItems",
+                newName: "CreatedUtc");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastModifiedUtc",
+                table: "TodoLists",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedUtc",
+                table: "TodoLists",
+                newName: "Created");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModifiedUtc",
+                table: "TodoItems",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedUtc",
+                table: "TodoItems",
+                newName: "Created");
+        }
+    }
+}
